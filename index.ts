17783bfd404b9dbc1452db51769eba65a62bb8f8
shantodey <ratul3330@gmail.com>
2026-07-13 01:49:48 +0600

    Refactor MongoDB connection and add endpoint to fetch doctors

--- index.ts
+++ index.ts
@@ -1,12 +1,58 @@
 import express, { Request, Response } from 'express';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
+const client = new MongoClient(process.env.MONGODB_URI!);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
+
+
+
+
+export async function connectToMongoDB() {
+    try {
+        await client.connect();
+        const db = client.db("DocConnect");
+        const doctorCollection = db.collection('doctors');
+
+        // Get all doctors
+        app.get("/doctors", async (req: Request, res: Response) => {
+            try {
+                const doctors = await doctorCollection.find().toArray();
+
+                res.status(200).json(doctors);
+            } catch (error) {
+                console.error(error);
+
+                res.status(500).json({
+                    message: "Failed to fetch doctors",
+                });
+            }
+        });
+
+        console.log("You successfully connected to MongoDB!");
+        return client;
+    } catch (err) {
+        console.dir(err);
+    }
+}
+
+// Call this only when your application terminates
+export async function disconnectFromMongoDB() {
+    await client.close();
+}
+
+
+
+
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
